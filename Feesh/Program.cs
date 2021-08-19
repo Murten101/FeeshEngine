afe8d7d32b811d3e723becb9f08d9d3c1e704e93
Murten101 <MaartenWirds@gmail.com>
2021-08-19 22:56:56 +0200

    BigBoi commmit
    hell yeah baby

---
+++ Feesh/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Windowing.Common;
+
+namespace Feesh
+{
+    class Program
+    {
+        private static Game game;
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Init();
+        }
+
+        private void Init()
+        {
+            using (game = new Game("Feesh"))
+            {
+                game.VSync = VSyncMode.Off;
+                game.Run();
+            }
+        }
+
+    }
+}
